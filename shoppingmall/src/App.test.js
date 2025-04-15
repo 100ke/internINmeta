72818fa68a7067fc6e530d6391965fbc67734b28
100ke <bhy9803@gmail.com>
2025-04-15 23:17:11 +0900

    250415 Jest로 테스트 전후 처리

--- shoppingmall/src/App.test.js
+++ shoppingmall/src/App.test.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { cleanup, fireEvent, render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders react link', () => {
+// 모든 테스트 끝난 뒤 DOM 초기화
+afterEach(()=>{
+  cleanup();
+});
+
+// 공통 렌더링을 beforeEach에서 처리
+beforeEach(()=>{
   render(<App />);
-  const linkElement = screen.getByText(/신발 상품 목록/i);
-  expect(linkElement).toBeInTheDocument();
+});
+
+test('상품 목록 제목이 나타난다', () => {
+  // const linkElement = screen.getByText(/신발 상품 목록/i);
+  // expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/신발 상품 목록/i)).toBeInTheDocument();
 });
 
 // 담기 버튼으로 인한 장바구니 갯수 변경 테스트
-test('상품 담기 버튼 클릭 시 장바구니 담은 갯수가 표시된다', async()=>{
-  render(<App/>);
-
-  const button = screen.getAllByText('담기')[0];
+test('담기 버튼 클릭 시 장바구니 담은 갯수가 표시된다', async()=>{
+  const button = screen.getAllByRole('button', {name:'담기'})[0];
   fireEvent.click(button);
+  fireEvent.click(button); // 2번 클릭
 
   // 비동기 처리 기다리기
   await waitFor(()=>{
-    expect(screen.getByText('1')).toBeInTheDocument();
+    expect(screen.getByText('2')).toBeInTheDocument();
   });
 });
