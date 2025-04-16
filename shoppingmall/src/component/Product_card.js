4969f88746044aee968da11b7876d9903917ebf6
100ke <bhy9803@gmail.com>
2025-04-17 02:26:38 +0900

    250417 장바구니 담긴 갯수 처리 수정

--- shoppingmall/src/component/Product_card.js
+++ shoppingmall/src/component/Product_card.js
@@ -6,26 +6,26 @@ function Product_card({setCartCount}) {
   const [isAdded, setIsAdded] = useState(false);
 
   const handleClick = () => {
-    // if (!isAdded) {
-    //   setIsAdded(true);
-    //   setCartCount(prev => prev + 1);
-    // } else {
-    //   setIsAdded(false);
-    //   setCartCount(prev => prev - 1);
-    // }
-
-    // 비동기 처리 흉내
     if (!isAdded) {
       setIsAdded(true);
-      setTimeout(()=>{
-        setCartCount(prev => prev + 1);
-      },500);
+      setCartCount(prev => prev + 1);
     } else {
       setIsAdded(false);
-      setTimeout(()=>{
-        setCartCount(prev => prev + 1);
-      },500);
+      setCartCount(prev => prev - 1);
     }
+
+    // 비동기 처리 흉내
+    // if (!isAdded) {
+    //   setIsAdded(true);
+    //   setTimeout(()=>{
+    //     setCartCount(prev => prev + 1);
+    //   },500);
+    // } else {
+    //   setIsAdded(false);
+    //   setTimeout(()=>{
+    //     setCartCount(prev => prev + 1);
+    //   },500);
+    // }
   };
 
   return (
