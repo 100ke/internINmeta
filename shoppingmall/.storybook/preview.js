84c7a9e6a6129308f54b10a14c20bbfc8e564ab5
100ke <bhy9803@gmail.com>
2025-05-06 22:19:13 +0900

    250506 스토리북 적용 실습

--- shoppingmall/.storybook/preview.js
+++ shoppingmall/.storybook/preview.js
@@ -1,10 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/App.css';
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
     controls: {
       matchers: {
        color: /(background|color)$/i,
-       date: /Date$/i,
+      //  date: /Date$/i,
+       date: /Date$/,
       },
     },
   },
